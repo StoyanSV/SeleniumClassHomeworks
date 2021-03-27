dd6b0cd6e3cf7920d0631144b6b158f191cead77
Stoyan SV <stoyan.vitekov@gmail.com>
2021-03-27 16:15:15 +0200

    working homework 6:not finished

---
+++ src/test/java/Homework6Demo/GuestOrder.java
@@ -0,0 +1,82 @@
+package Homework6Demo;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import javax.swing.*;
+
+public class GuestOrder {
+
+    WebDriver driver;
+
+    public void GuestOrderTests() {
+    }
+
+    @BeforeMethod
+    public void setupBrowser() {
+        System.setProperty("webdriver.chrome.driver", "C:\\DEV\\selenium_drivers\\chromedriver.exe");
+        this.driver = new ChromeDriver();
+        this.driver.manage().window().maximize();
+        this.driver.get("http://shop.pragmatic.bg/index.php?route=product/product&product_id=40");
+    }
+
+    @Test
+    public void successfulPlacementOfGuestOrder() {
+        WebElement addToCartButton = this.driver.findElement(By.id("button-cart"));
+        addToCartButton.click();
+        WebElement miniShoppingCart = this.driver.findElement(By.cssSelector("#cart button.btn-block"));
+        miniShoppingCart.click();
+        WebDriverWait wait = new WebDriverWait(this.driver, 10L);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("ul.pull-right div a:nth-of-type(2)")));
+        WebElement miniCartCheckoutLink = this.driver.findElement(By.cssSelector("ul.pull-right div a:nth-of-type(2)"));
+        miniCartCheckoutLink.click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.radio:nth-of-type(2) input")));
+        WebElement guestRadioButton = this.driver.findElement(By.cssSelector("div.radio:nth-of-type(2) input"));
+        guestRadioButton.click();
+        WebElement continueButton = this.driver.findElement(By.id("button-account"));
+        continueButton.click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("button-guest")));
+        this.driver.findElement(By.id("input-payment-firstname")).sendKeys(new CharSequence[]{"NqkvoFirstName"});
+        this.driver.findElement(By.id("input-payment-lastname")).sendKeys(new CharSequence[]{"NqkyvLastName"});
+        String randomEmail = RandomStringUtils.randomAlphanumeric(7) + "@blabla.com";
+        this.driver.findElement(By.id("input-payment-email")).sendKeys(new CharSequence[]{randomEmail});
+        this.driver.findElement(By.id("input-payment-telephone")).sendKeys(new CharSequence[]{"1234567890"});
+        this.driver.findElement(By.id("input-payment-address-1")).sendKeys(new CharSequence[]{"nqkav adress 1"});
+        this.driver.findElement(By.id("input-payment-city")).sendKeys(new CharSequence[]{"Mugla"});
+        this.driver.findElement(By.id("input-payment-postcode")).sendKeys(new CharSequence[]{"1234"});
+        Select countryDropDown = new Select(this.driver.findElement(By.id("input-payment-country")));
+        countryDropDown.selectByValue("33");
+        wait.until(ExpectedConditions.textToBePresentInElementLocated(By.id("input-payment-zone"), "Yambol"));
+        Select regionDropDown = new Select(this.driver.findElement(By.id("input-payment-zone")));
+        regionDropDown.selectByValue("496");
+        this.driver.findElement(By.id("button-guest")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".panel-body > p:nth-of-type(4) > .form-control")));
+        WebElement field = this.driver.findElement(By.cssSelector(".panel-body > p:nth-of-type(4) > .form-control"));
+        field.click();
+        field.sendKeys("text, explaining that the door of my house is on the opposite site of the building");
+        this.driver.findElement(By.id("button-shipping-method")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name='agree']")));
+        this.driver.findElement(By.xpath("//input[@name='agree']")).click();
+        this.driver.findElement(By.cssSelector("#button-payment-method")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".table >tfoot>tr:nth-of-type(3)>td:nth-of-type(2)")));
+        this.driver.findElement(By.cssSelector(".table >tfoot>tr:nth-of-type(3)>td:nth-of-type(2)"));
+        this.driver.findElement(By.id("button-confirm")).click();
+        // System.out.printf(this.driver.findElement(By.cssSelector("#content >h1")).getAttribute();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#content > p:nth-of-type(1)")));
+        Assert.assertEquals(this.driver.findElement(By.cssSelector("#content > p:nth-of-type(1)")).getText(),"Your order has been successfully processed!");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+    }
+}
